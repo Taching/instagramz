3a81a3b17af8ba235cb5f17e53bd46a10ade868c
Takatoshi Wada <fueledbytaching@gmail.com>
2021-07-15 13:52:46 +0800

    test error

--- functions/instagram.js
+++ functions/instagram.js
@@ -6,26 +6,30 @@ const cache = {
   lastFetch: 0,
   posts: [],
 }
+function slimUpPosts(response) {
+  return response.data.user.edge_owner_to_timeline_media.edges.map(edge => ({
+    biggie: edge.node.thumbnail_src,
+    thumbnail: edge.node.thumbnail_resources[2].src,
+    url: `https://instagram.com/p/${edge.node.shortcode}`,
+    caption:
+      edge.node.edge_media_to_caption.edges.length > 0
+        ? edge.node.edge_media_to_caption.edges[0].node.text
+        : null,
+    id: edge.node.id,
+  }))
+}
 async function getPosts() {
-  //first see if we have cache in 30mins
+  // first see if we have cache in 30mins
   const timeSinceLastFetch = Date.now() - cache.lastFetch
   if (timeSinceLastFetch <= 1800000) {
     return cache.posts
   }
-  const data = await fetch(url).then(res => res.json())
+  const data = await fetch(url).then(res)
   console.log(data)
   const posts = slimUpPosts(data)
   cache.lastFetch = Date.now()
   cache.posts = posts
   return posts
-}
-function slimUpPosts(response) {
-  return response.data.user.edge_owner_to_timeline_media.edges.map(edge => ({
-    thumbnail: edge.node.thumbnail_src,
-    url: `https://instagram.com/p/${edge.node.shortcode}`,
-    caption: edge.node.edge_media_to_caption.edges[0].node.text,
-    id: edge.node.id,
-  }))
 }
 exports.handler = async function (event, context, callback) {
   const posts = await getPosts()
